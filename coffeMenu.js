b6f6d499ab0a1afa5cb2f22813349317f1832ca6
david718 <hkc7180@gmail.com>
2018-12-22 10:52:24 +0900

    Create coffeMenu.js

---
+++ coffeMenu.js
@@ -0,0 +1,8 @@
+var coffeeMenu = [
+    { menu : '아메리카노', price : '3.500' },
+    { menu : '카페라떼', price : '4,000' },
+    { menu : '카푸치노', price : '4,000' },
+    { menu : '민트티', price : '4,000' },
+    { menu : '밀크티', price : '4,500' },
+    { menu : '플랫 화이트', price : '5,000' }
+]
